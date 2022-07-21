6174f67b5c10ab216e830f4073334a131d2185d1
siraj123456 <srjberryy@gmail.com>
2022-07-21 09:56:19 +0200

    fixing Backend

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ var express = require('express');
 var app = express();
 const request = require('request')
 const api_helper = require('./API_helper')
-
+// const  { Server } = require("socket.io");
+const http = require('http');
+// const server = http.createServer(app);
+// const io = new Server(server)
+const io = require("socket.io")(8081, {
+    path: "/"
+  });
 const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
 
+const INDEX = "\\Index.html";
+
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send(__dirname+INDEX);
 })
 
 app.get('/LuccaLeaves/:ownerId&:date&:paging', (req, res) => {
@@ -25,7 +33,6 @@ app.get('/LuccaLeaves/:ownerId&:date&:paging', (req, res) => {
 
     api_helper.make_API_call('https://i-tracing.ilucca-test.net/api/v3/leaves?leavePeriod.ownerId=' + ownerId + '&date=' + date + '&paging=' + paging)
         .then(response => {
-            console.log("res: ", response)
             res.json(response)
         })
         .catch(error => {
@@ -33,47 +40,44 @@ app.get('/LuccaLeaves/:ownerId&:date&:paging', (req, res) => {
         })
 })
 
-// app.post('/webhook', (req, res) => {
-//     res.set('Access-Control-Allow-Origin', ['*']);
-//     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.append('Access-Control-Allow-Headers', 'Content-Type');
-
-//     request.post("http://localhost:3000/webhook", {
-//         formData: {"user":"KJHKJH","password":"SFD"}, rejectUnauthorized: false
-//     }, function optionalCallback(err, httpResponse, b) {
-//         if (err) {
-//             return console.error('upload failed:', err);
-//         }
-//         res.send(b)
-//     });
-// })
-
-
-app.post('/webhook', function(req, res){
-    let myJson = req.body;    
-    console.log("myJson", myJson)
-      res.send(myJson);	 // echo the result back
-  });
-
-
-app.get("/posts", (req, res) => {
-    res.set('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-
-    request.get("https://jsonplaceholder.typicode.com/posts", {
-        formData: {
-            body: "body",
-            title: "title",
-            userId: "userId"
-        }, rejectUnauthorized: false
-    }, function optionalCallback(err, httpResponse, b) {
-        if (err) {
-            return console.error('upload failed:', err);
-        }
-        res.send(b)
-    });
+io.on('connection' ,(socket)=>{
+    console.log('a user connected!')
+    socket.on('chat message', msg=>{
+        io.emit('chat message', "msg: "+msg+", myJSON: ")
+    })
 })
+
+app.use(express.json());
+
+app.post('/webhook', function(req, res){//Very Important: we receive only the confirmed actions: creation et annulation APPROVEE SEULEMENT
+    console.log(req.body); 
+    var postData = JSON.stringify(req.body);
+
+    var requestOptions = {
+        hostname: 'localhost', // url or ip address
+        port: 8088, // default to 80 if not provided
+        path: '/integrate',
+        method: 'POST', // HTTP Method
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        }
+    };
+    var externalRequest = http.request(requestOptions, (externalResponse) => {
+        externalResponse.setEncoding('utf8');
+        externalResponse.on('data', (chuck) => {
+        });
+        // ServerB done responding
+        externalResponse.on('end', () => {
+            // Response to client
+            res.end('data was send to serverB');
+        });
+    });
+    
+    // Free to send anthing to serverB
+    externalRequest.write(postData);
+    externalRequest.end();
+  });
 
 app.listen(process.env.PORT || 8080, function () {
     console.log("Backend Application listening at 8080")
